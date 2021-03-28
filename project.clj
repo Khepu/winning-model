4aa0705a20a9946a7adc57b1402ba8271656dbc0
Giorgos Makris <gmakris@datascouting.com>
2021-03-28 13:01:11 +0300

    Feature: Added cuda & ui deps

--- project.clj
+++ project.clj
@@ -4,11 +4,14 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
 
+  :mirrors {"maven-restlet" {:url "https://maven.restlet.org"}}
+
   :dependencies [[org.clojure/clojure "1.10.0"]
 
                  ;;dl4j
-                 [org.nd4j/nd4j-cuda-10.2 "1.0.0-beta7"]
+                 [org.nd4j/nd4j-cuda-10.1                "1.0.0-beta7"]
                  [org.deeplearning4j/deeplearning4j-core "1.0.0-beta7"]
+                 [org.deeplearning4j/deeplearning4j-ui   "1.0.0-beta7"]
 
                  ;;logging
                  [org.slf4j/slf4j-api "1.7.30"]
