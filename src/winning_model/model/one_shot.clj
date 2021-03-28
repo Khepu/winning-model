cb2e07e5fa2540da0e7267f8947e7b0812eeee30
Giorgos Makris <gmakris@datascouting.com>
2021-03-28 14:53:50 +0300

    Chore: Model file

---
+++ src/winning_model/model/one_shot.clj
@@ -0,0 +1,8 @@
+(ns winning-model.model.one-shot
+  (:import
+   [org.deeplearning4j.nn.conf ComputationGraphConfiguration]
+   [org.deeplearning4j.nn.conf NeuralNetConfiguration]))
+
+(defn create-model
+  [parameters]
+  nil)
